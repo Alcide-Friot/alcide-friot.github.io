1c329356bca510c3dcd4d511611d9a25b266ba05
Alcide Friot <alcide.friot@outlook.fr>
2025-12-14 17:07:03 -0500

    All done
    Just needs the content now

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-﻿
+﻿const sections = document.querySelectorAll('.section');
+const closeButton = document.querySelector('.panel-close');
+const panels = document.querySelectorAll('.content-panel');
+
+function closePanels() {
+    panels.forEach(panel => panel.classList.remove('active'));
+    closeButton.classList.remove('active');
+}
+
+sections.forEach(section => {
+    section.addEventListener('click', e => {
+        e.preventDefault();
+
+        const target = section.dataset.panel;
+
+        panels.forEach(panel => panel.classList.remove('active'));
+
+        const panel = document.querySelector(
+            `.content-panel[data-panel="${target}"]`
+        );
+
+        if (panel) {
+            panel.classList.add('active');
+            closeButton.classList.add('active');
+        }
+    });
+});
+
+closeButton.addEventListener('click', closePanels);
